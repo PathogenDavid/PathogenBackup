13025d8b09b3b12203cfa2bdc067657803c4d10c
David Maas <contact@pathogenstudios.com>
2014-11-16 12:42:10 -0600

    Started skeleton Visual Studio NMake project. (Code is from a devkitPro example.)

---
+++ intellifix.h
@@ -0,0 +1,12 @@
+// This file contains fixes to make Intellisense happy
+#ifndef __INTELLIFIX_H__
+#define __INTELLIFIX_H__
+
+#define true 1
+#define false 0
+#define __extension__
+#define __attribute__(x)
+
+typedef void* __builtin_va_list;
+
+#endif
